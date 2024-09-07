5265dce827d0a5e95ca31fdc1042e27d2f214294
mohamed hossam <wawmmm9@gmail.com>
2024-09-07 20:21:33 +0300

    added the users sql file and the notepad for users test

---
+++ HotelBooking/SQL File/03.sql
@@ -0,0 +1,19 @@
+INSERT INTO hotel.user (id, first_name, last_name, email, res_room, wallet) 
+VALUES
+(1, 'Alice', 'Johnson', 'alice.johnson@example.com', 1, 5000),
+(2, 'Bob', 'Smith', 'bob.smith@example.com', 2, 5000),
+(3, 'Charlie', 'Brown', 'charlie.brown@example.com', 3, 5000),
+(4, 'David', 'Williams', 'david.williams@example.com', 4, 5000),
+(5, 'Eva', 'Davis', 'eva.davis@example.com', 5, 5000),
+(6, 'Frank', 'Miller', 'frank.miller@example.com', 6, 5000),
+(7, 'Grace', 'Wilson', 'grace.wilson@example.com', 7, 5000),
+(8, 'Henry', 'Moore', 'henry.moore@example.com', 8, 5000),
+(9, 'Ivy', 'Taylor', 'ivy.taylor@example.com', 9, 5000),
+(10, 'Jack', 'Anderson', 'jack.anderson@example.com', 10, 5000),
+(11, 'David', 'Williams', 'david.williams@example.com', 11, 5000),
+(12, 'Lucas', 'Garcia', 'lucas.garcia@example.com', 12, 5000),
+(13, 'Maria', 'Martinez', 'maria.martinez@example.com', 13, 5000),
+(14, 'Nina', 'Thompson', 'nina.thompson@example.com', 14, 5000),
+(15, 'Oliver', 'Walker', 'oliver.walker@example.com', 15, 5000),
+(16, 'Paul', 'Young', 'paul.young@example.com', 16, 5000),
+(17, 'Quinn', 'Adams', 'quinn.adams@example.com', 17, 5000);
